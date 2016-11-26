888d70f58a959a29f204a0d49e3b744a46fa7890
loganmackenzie <loganmackenzie1@gmail.com>
2016-11-26 08:35:46 -0500

    Add files via upload

---
+++ CivArts_ArtsSelector/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace CivArts_ArtsSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.select_images_button = new System.Windows.Forms.Button();
+            this.title_label = new System.Windows.Forms.Label();
+            this.next = new System.Windows.Forms.Button();
+            this.mark_location = new System.Windows.Forms.Button();
+            this.show_title = new System.Windows.Forms.Button();
+            this.show_image = new System.Windows.Forms.Button();
+            this.picture_file_selector = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.MaximumSize = new System.Drawing.Size(1382, 744);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.select_images_button);
+            this.splitContainer1.Panel2.Controls.Add(this.title_label);
+            this.splitContainer1.Panel2.Controls.Add(this.next);
+            this.splitContainer1.Panel2.Controls.Add(this.mark_location);
+            this.splitContainer1.Panel2.Controls.Add(this.show_title);
+            this.splitContainer1.Panel2.Controls.Add(this.show_image);
+            this.splitContainer1.Size = new System.Drawing.Size(886, 407);
+            this.splitContainer1.SplitterDistance = 365;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.TabStop = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(886, 365);
+            this.splitContainer2.SplitterDistance = 443;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(439, 365);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // select_images_button
+            // 
+            this.select_images_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.select_images_button.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.select_images_button.Location = new System.Drawing.Point(351, 2);
+            this.select_images_button.Name = "select_images_button";
+            this.select_images_button.Size = new System.Drawing.Size(100, 30);
+            this.select_images_button.TabIndex = 5;
+            this.select_images_button.Text = "Select Images";
+            this.select_images_button.UseVisualStyleBackColor = true;
+            this.select_images_button.Click += new System.EventHandler(this.select_images_button_Click);
+            // 
+            // title_label
+            // 
+            this.title_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.title_label.AutoSize = true;
+            this.title_label.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_label.Location = new System.Drawing.Point(12, 8);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(48, 21);
+            this.title_label.TabIndex = 4;
+            this.title_label.Text = "Title";
+            this.title_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // next
+            // 
+            this.next.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.next.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.Location = new System.Drawing.Point(774, 2);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(100, 30);
+            this.next.TabIndex = 3;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // mark_location
+            // 
+            this.mark_location.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.mark_location.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mark_location.Location = new System.Drawing.Point(668, 2);
+            this.mark_location.Name = "mark_location";
+            this.mark_location.Size = new System.Drawing.Size(100, 30);
+            this.mark_location.TabIndex = 2;
+            this.mark_location.Text = "Mark Location";
+            this.mark_location.UseVisualStyleBackColor = true;
+            this.mark_location.Click += new System.EventHandler(this.mark_location_Click);
+            // 
+            // show_title
+            // 
+            this.show_title.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.show_title.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.show_title.Location = new System.Drawing.Point(562, 2);
+            this.show_title.Name = "show_title";
+            this.show_title.Size = new System.Drawing.Size(100, 30);
+            this.show_title.TabIndex = 1;
+            this.show_title.Text = "Show Title";
+            this.show_title.UseVisualStyleBackColor = true;
+            this.show_title.Click += new System.EventHandler(this.show_title_Click);
+            // 
+            // show_image
+            // 
+            this.show_image.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.show_image.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.show_image.Location = new System.Drawing.Point(456, 2);
+            this.show_image.Name = "show_image";
+            this.show_image.Size = new System.Drawing.Size(100, 30);
+            this.show_image.TabIndex = 0;
+            this.show_image.Text = "Show Image";
+            this.show_image.UseVisualStyleBackColor = true;
+            this.show_image.Click += new System.EventHandler(this.show_image_Click);
+            // 
+            // picture_file_selector
+            // 
+            this.picture_file_selector.FileName = "picture_file_selector";
+            this.picture_file_selector.Multiselect = true;
+            this.picture_file_selector.Title = "Select All Images Desired";
+            this.picture_file_selector.FileOk += new System.ComponentModel.CancelEventHandler(this.picture_file_selector_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 407);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Grove City College Huma 301: Civ-Arts Arts Selector";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button mark_location;
+        private System.Windows.Forms.Button show_title;
+        private System.Windows.Forms.Button show_image;
+        private System.Windows.Forms.Label title_label;
+        private System.Windows.Forms.Button select_images_button;
+        private System.Windows.Forms.OpenFileDialog picture_file_selector;
+
+    }
+}
+
---
+++ CivArts_ArtsSelector/Form1.cs
@@ -0,0 +1,172 @@
+﻿/*
+ * Project: Civ-Arts Arts Selector
+ * Author: Logan MacKenzie
+ * Date:  May 2016 - June 2016
+ * Description: The Civ-Arts Arts Selector is a study tool to assist in studying
+ *          pieces of art. Specifically, this tool will assist in identifying 
+ *          specific pieces of art allowing the user to better remember them.
+ * 
+ * Updated: November 26, 2016
+ * Updater: Logan MacKenzie
+ * Description: The original Civ-Arts Arts Selector simply drew images from a 
+ *          pre-prepared folder of images. This update added additional code 
+ *          permitted the user to chose specific images from the folder to study.
+ *          This allows for studying for specific tests, before having been 
+ *          introduced to all the images.
+ *              Additionally, this update allows the user to find and modify the 
+ *          the folder to suite the specific class.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CivArts_ArtsSelector
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random(); // Random number generator. Time dependent seed
+        Image image;         // Holds the current image to display
+        Rectangle rectangle; // Outlines the selection
+        string[] dir;        // Holds the list of files in the Pictures folder.
+        int current_image;   // Holds the current index in the directory of the current image.
+
+        public Form1()
+        {
+            InitializeComponent(); // Initialize buttons, picture boxes, etc.
+            
+            // Get a listing of all the images in the Pictures directory!
+            dir = System.IO.Directory.GetFiles("Pictures");  
+
+            // Call next_Click to initialize the image and display a segment
+            next_Click(new Object(), new EventArgs()); 
+        }
+
+        // next_Click randomly chooses the next image and reads it in from the file. 
+        // It randomly selects a portion of the image to display and clears the title and full-image picture box.
+        private void next_Click(object sender, EventArgs e)
+        {
+            // Maximaze the pictureBox before inserting image.
+            pictureBox1.Height = splitContainer2.Height; 
+            pictureBox1.Width = splitContainer2.SplitterDistance;
+
+            // Randomly choose a image and import it.
+            current_image = random.Next(dir.Length);    // Choose a random image
+            image = Image.FromFile(dir[current_image]); // Import from the file.
+            
+            // Randomly choose a width and a starting location. The height is proportional to the width. 
+            rectangle.Width = Convert.ToInt32(image.Width / (2 * (random.NextDouble() + 1)));
+            rectangle.Height = Convert.ToInt32(image.Height * rectangle.Width / image.Width);
+            rectangle.X = random.Next(image.Width - rectangle.Width - image.Width / 250 - 1); // Leave some space around the edges to display the rectangle
+            rectangle.Y = random.Next(image.Height - rectangle.Height - image.Width / 250 - 1); // Leave some space around the edges to display the rectangle
+
+            // Display the selected region of the image
+            Bitmap sourceBitmap = new Bitmap(image); // Convert the image to Bitmap and then clone the selection into the pictureBox.
+            pictureBox1.Image = sourceBitmap.Clone(rectangle, sourceBitmap.PixelFormat);
+
+            // Hide the full image picture box and the title.
+            pictureBox2.Visible = false; // Hide the full-image box.
+            title_label.Text = "";       // Clear title of work
+            
+            // Collect garbage. The images are large and can crash the program
+            // If care is not taken to clean up frequently.
+            GC.Collect();   // Clean up the garbage (makes sure the program does not run out of memory).
+        }
+
+        // Displays the title of the piece of art.
+        // show_title_Click gets the image title (just the filename) for
+        // the current image. It then removes the file path (if present)
+        // and displays the title along the bottom of the screen.
+        private void show_title_Click(object sender, EventArgs e)
+        {
+            string long_title = dir[current_image];
+            int pos = long_title.LastIndexOf('\\');
+            //int pos = long_title.IndexOf("Pictures\\");
+            if (pos == -1)
+                title_label.Text = long_title;
+            else
+                title_label.Text = long_title.Remove(0, pos + 1); // Remove the file path from the name.
+        }
+
+        // Displays the full image 
+        // show_image_Click just reveals the entire image allowing the user to 
+        // study it and figure out where the selection was taken from.
+        private void show_image_Click(object sender, EventArgs e)
+        {
+            // Reset the size of the picture box in case it was shrunk for 
+            // a previous image.
+            pictureBox2.Height = splitContainer2.Height;
+            pictureBox2.Width = splitContainer2.Width - splitContainer2.SplitterDistance;
+            // Display the image.
+            pictureBox2.Image = image;
+            pictureBox2.Visible = true;
+        }
+
+        // Displays where the selection was taken from the image.
+        // mark_location_Click extracts a Bitmap image from the original image
+        // and calls AddRectangle to show where the selected portion was taken
+        // from. Then it makes pictureBox2 visible to display the entire image.
+        private void mark_location_Click(object sender, EventArgs e)
+        {
+            Bitmap sourceBitmap = new Bitmap(image);
+            pictureBox2.Image = AddRectangle(sourceBitmap, rectangle);
+            pictureBox2.Visible = true;
+            GC.Collect(); // Clean up (or else the program crashes after running out of memory)
+        }
+
+        // Sketches the borders of the selected rectangle of the image.
+        // AddRectangle essetially sketches the border in red by changing every pixel along
+        // the borders to be red. This method was chosen over just sketching a rectangle on
+        // the image because scaling the rectangle was difficult to keep consistent with 
+        // changing screen sizes.
+        private Bitmap AddRectangle(Bitmap bmap, Rectangle rect)
+        {
+            int line_width = bmap.Width / 250; // Scale border width to size of image so it appears consistent.
+            for (int i = 0; i < line_width; i++)
+            {
+                for (int x = rect.X; x < rect.X + rect.Width; x++) // Top and bottom borders
+                {   // Set all the pixels along the top and bottom borders to be red.
+                    bmap.SetPixel(x, rect.Y+i, Color.Red); // Top border
+                    bmap.SetPixel(x, rect.Y+i + rect.Height, Color.Red); // Bottom border
+                }
+                for (int y = rect.Y; y < rect.Y + rect.Height; y++) // Left and right borders
+                {   // Set all the pixels along the left and right borders to be red.
+                    bmap.SetPixel(rect.X + i, y, Color.Red); // Left border
+                    bmap.SetPixel(rect.X + i + rect.Width, y, Color.Red); // Right border
+                }
+            }
+            return bmap;
+        }
+
+        // select_images_button_Click opens a standard Windows file explorer window
+        // allowing the user to select all the desired images to study.
+        private void select_images_button_Click(object sender, EventArgs e)
+        {
+            picture_file_selector.InitialDirectory = System.IO.Directory.GetCurrentDirectory() + "\\Pictures";
+            picture_file_selector.ShowDialog();
+            next_Click(new Object(), new EventArgs()); // Call next_Click to select a new picture.
+            GC.Collect();
+        }
+
+        // After the user has selected all the desired images and clicks Ok, this
+        // function runs. picture_file_selector_FileOk extracts the file names selected
+        // by the user and appends "Pictures\\" to them so the program can find the
+        // pictures in their folder. It sets the pictures directory from which all
+        // images are drawn to consist of the file selected.
+        // Note: This only works within the pre-supplied folder due to the folder path.
+        private void picture_file_selector_FileOk(object sender, CancelEventArgs e)
+        {
+            OpenFileDialog opfldlg = (OpenFileDialog)sender;
+            dir = opfldlg.SafeFileNames; // Get the selected image file names.
+            for (int i = 0; i < opfldlg.SafeFileNames.Length; i++)
+            {   // Append the folder name on the front of the image file name.
+                dir[i] = "Pictures\\" + dir[i];
+            }
+        }
+    }
+}
